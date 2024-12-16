09a386e93acfd62a16f9c65ffce4ff57cc18eba4
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-12-15 22:06:53 -0300

    Snapshot, want to try a new approach

--- index.js
+++ index.js
@@ -10,9 +10,33 @@ function readFlags(){}
 function fetchUserActivity(user){
     fetch(`https://api.github.com/users/${user}/events`)
     .then(res => res.json())
-    .then(result => objetos = result);
+    .then(result => console.log(result));
 }
 
+function organizeData(data){
+    //Index of eventTypes is correlated to index of eventTypeCount
+    let eventTypes = [];
+    let eventTypeCount = [];
+
+    //Keeps track of every type of event and the number of times they appear
+    for (let event of data){
+        if (!eventTypes.includes(event.type)){
+            eventTypes.push(event.type);
+            eventTypeCount.push(1);
+        }else {
+            eventTypeCount[eventTypes.indexOf(event.type)]++;
+        }
+    }
+
+    console.log(`Se recibieron los eventos ${eventTypes} con la cantidad de ${eventTypeCount}, respectivamente`)
+
+    outputData({eventTypes: eventTypes, eventTypeCount: eventTypeCount});
+}
+
+function outputData(data){
+    console.log(`-- Let's check ${args[2]}'s activity!`)
+
+}
 
 function readArgumentsList(){
     const user = args[2];
@@ -26,7 +50,7 @@ function readArgumentsList(){
     }
 
     fetchUserActivity(user);
+    
 }
 
-
-fetchUserActivity(args[2]);
+readArgumentsList();
