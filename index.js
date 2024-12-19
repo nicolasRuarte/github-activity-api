42a3aad20e4fe115db286b24d6a99b06c30ae22d
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-12-18 21:39:18 -0300

    Added error handling

--- index.js
+++ index.js
@@ -4,94 +4,76 @@ import process from "node:process"
 //gets the argument list when the command is used.
 const args = process.argv;
 
-function readFlags(){}
-
-function organizeData(data){
-    let lastEventType = "";
-    let lastEventTypeCounter = 0;
-    let lastRepo = "";
-    for (ghubEvent of data){
-        if (ghubEvent.type != lastEventType){
-            pickCorrectOutputMessage(lastEventType, lastEventTypeCounter, lastRepo);
-            lastEventType = ghubEvent.type;
-            lastEventTypeCounter = 0;
-            lastRepo = ghubEvent.repo.name;
+function outputEvents(data){
+    console.log(`${data[0].actor.login}'s activity during the last 90 days was the following:`)
+    for (let event of data){
+        switch (event.type){
+            case "CommitCommentEvent":
+                console.log(`- ${event.payload.action} a commit comment on ${event.repo.name}`)
+                break;
+            case "CreateEvent":
+                console.log(`- Created a ${event.payload.ref_type} on ${event.repo.name}`)
+                break;
+            case "DeleteEvent":
+                console.log(`- Deleted a ${event.payload.ref_type} on ${event.repo.name}`)
+                break;
+            case "ForkEvent":
+                console.log(`- Forked a repo to ${event.payload.forkee}`);
+                break;
+            case "GollumEvent":
+                console.log(`- Updated the pages ${event.payload.pages} from the ${event.repo.name}'s wiki.`)
+                break;
+            case "IssueCommentEvent":
+                console.log(`- ${event.payload.action} an issue comment on ${event.repo.name}`)
+                break;
+            case "IssuesEvent":
+                console.log(`- ${event.payload.action} an issue on ${event.repo.name}`)
+                break;
+            case "MemberEvent":
+                console.log(`- ${event.payload.action} ${event.payload.member} as a member of ${event.repo.name}`)
+                break;
+            case "PublicEvent":
+                console.log(`- Made one of his private repositories public`)
+                break;
+            case "PullRequestEvent":
+                console.log(`- ${event.payload.action} ${event.payload.number} pull requests on ${event.repo.name}`)
+                break;
+            case "PullRequestReviewEvent":
+                console.log(`- ${event.payload.action} a PR review on the PR ${event.payload.pull_request} from ${event.repo.name}`)
+                break;
+            case "PullRequestReviewCommentEvent":
+                console.log(`- ${event.payload.action} a PR Review comment on the PR ${event.payload.pull_request} from ${event.repo.name}`)
+                break;
+            case "PullRequestReviewThreadEvent":
+                console.log(`- ${event.payload.action} a PR review thread on the PR ${event.payload.pull_request} from ${event.repo.name}`)
+                break;
+            case "PushEvent":
+                console.log(`- Pushed ${event.payload.size} commits to ${event.repo.name}`)
+                break;
+            case "ReleaseEvent":
+                console.log(`- ${event.payload.action} a release on ${event.repo.name}`)
+                break;
+            case "SponsorshipEvent":
+                console.log(`- Sponsored the repository ${event.repo.name}`)
+                break;
+            case "WatchEvent":
+                console.log(`- Starred the repository ${event.repo.name}`)
+                break;
+            default:
+                break;
         }
     }
 }
 
 function fetchUserActivity(user){
-    fetch(`https://api.github.com/users/${user}/events`)
-    .then(res => res.json())
-    .then(result => {
-        console.log(result);
-        organizeData(result);
-    });
-
-}
-
-function pickCorrectOutputMessage(eventType, quantity, repo){
-    switch (eventType){
-        case "CommitCommentEvent":
-            console.log(`- Made ${quantity} commit comments on ${repo}`)
-            break;
-        case "CreateEvent":
-            console.log(`- Made ${quantity} tags or branches on ${repo}`)
-            break;
-        case "DeleteEvent":
-            console.log(`- Deleted ${quantity} tags or branches on ${repo}`)
-            break;
-        case "ForkEvent":
-            console.log(`- Forked ${quantity} repositories`);
-            break;
-        case "GollumEvent":
-            console.log(`- Updated or created ${quantity} the wiki page on ${repo}`)
-            break;
-        case "IssueCommentEvent":
-            console.log(`- Commented ${quantity} times on issues/pull requests from ${repo}`)
-            break;
-        case "IssuesEvent":
-            console.log(`- Updated ${quantity} issues on ${repo}`)
-            break;
-        case "MemberEvent":
-            console.log(`- Made public the repo: ${repo}`)
-            break;
-        case "PublicEvent":
-            console.log(`- Made ${quantity} tags or branches on ${repo}`)
-            break;
-        case "PullRequestEvent":
-            console.log(`- Updated or creaded ${quantity} pull requests on ${repo}`)
-            break;
-        case "PullRequestReviewEvent":
-            console.log(`- Created ${quantity} pull request reviews on ${repo}`)
-            break;
-        case "PullRequestReviewCommentEvent":
-            console.log(`- Commented ${quantity} times on pull request reviews from ${repo}`)
-            break;
-        case "PullRequestReviewThreadEvent":
-            console.log(`- Edited ${quantity} pull request threads on ${repo}`)
-            break;
-        case "PushEvent":
-            console.log(`- Pushed ${quantity} commits on ${repo}`)
-            break;
-        case "ReleaseEvent":
-            console.log(`- Made ${quantity} tags or branches on ${repo}`)
-            break;
-        case "SponsorshipEvent":
-            console.log(`- Made ${quantity} tags or branches on ${repo}`)
-            break;
-        case "WatchEvent":
-            console.log(`- Made ${quantity} tags or branches on ${repo}`)
-            break;
-        default:
-            break;
+    try{
+        fetch(`https://api.github.com/users/${user}/events`)
+        .then(res => res.json())
+        .then(result => outputEvents(result));
     }
-}
-
-
-function outputData(data){
-    console.log(`-- Let's check ${args[2]}'s activity!`)
-
+    catch (error){
+        console.error(error);
+    }
 }
 
 function readArgumentsList(){
